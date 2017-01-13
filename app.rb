a2f5bc2e5e2c5c2a8125b08a7606f94ef855d691
Michael Murphy <mmurphy@jobcase.com>
2017-01-13 10:56:44 -0500

    Add advice action
    This used the http://api.adviceslip.com/ api

--- app.rb
+++ app.rb
@@ -7,13 +7,18 @@ post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
 
   action, repo = message.split('_').map {|c| c.strip.downcase }
-  repo_url = "https://api.github.com/repos/#{repo}"
 
   case action
     when 'issues'
+      repo_url = "https://api.github.com/repos/#{repo}"
       resp = HTTParty.get(repo_url)
       resp = JSON.parse resp.body
       respond_message("There are #{resp['open_issues_count']} open issues on #{repo}", resp['avatar_url'])
+    when 'advice'
+      repo_url = "http://api.adviceslip.com/advice"
+      resp = HTTParty.get(repo_url)
+      resp = JSON.parse resp.body
+      respond_message("*Your tip:* \n resp['advice']")
   end
 end
 
